72dd15addb96352c9e345d9df6e3919f3fe23628
ABeliy <andreybelobra2010@gmail.com>
2023-10-04 22:15:28 +0300

    font upgrade

--- app/layout.tsx
+++ app/layout.tsx
@@ -27,6 +27,12 @@ export default function RootLayout({
           type='image/<generated>'
           sizes='<generated>'
         />
+        <link
+          href='https://fonts.googleapis.com/css2?family=Nunito+Sans:opsz,wght@6..12,200&display=swap'
+          rel='stylesheet'
+          type='text/css'
+        />
+
         <link rel='icon' href='/favicon.ico' sizes='any' />
       </head>
       <body className={inter.className}>{children}</body>
