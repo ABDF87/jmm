9c1b3a37ecc5161cb72445859d5fd6424302e39e
ABeliy <andreybelobra2010@gmail.com>
2023-10-16 19:42:50 +0300

    set design credits

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import styles from './styles/Footer.module.css';
 
 const Footer = () => {
-  return <div className={styles.mainContainer}>2023 © Jean-Marc Mazzoni</div>;
+  return <div className={styles.mainContainer}>2023 © Website:Andrey Belobra</div>;
 };
 
 export default Footer;
