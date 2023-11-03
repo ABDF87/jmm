352bf2aa3506e567afd6268ccb3e7705536a7787
ABeliy <andreybelobra2010@gmail.com>
2023-11-03 10:54:11 +0200

    keywords SEO

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,6 +7,8 @@ const inter = Inter({ subsets: ['latin'] });
 export const metadata: Metadata = {
   title: 'Jean-Marc Mazzoni - Food photographer',
   description: 'Food photography portfolio',
+  keywords:
+    'food photography, food photography for restourants, photographer portfolio, photographer food, food photographer, JM Mazzoni, Jean-Marc Mazzoni, food photo, food photo for restourants',
 };
 
 export default function RootLayout({
@@ -21,10 +23,8 @@ export default function RootLayout({
           href='https://fonts.googleapis.com/css2?family=Nunito+Sans:opsz,wght@6..12,200&display=swap'
           rel='stylesheet'
           type='text/css'
-        /> 
-        <link rel="icon" href="/favicon.ico"></link>
-        
-
+        />
+        <link rel='icon' href='/favicon.ico'></link>
       </head>
       <body className={inter.className}>{children}</body>
     </html>
