130e377e033bb300adcf1f0ab66c3eb0354d197b
ABeliy <andreybelobra2010@gmail.com>
2025-01-13 19:28:51 +0200

    about fixed

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -3,52 +3,62 @@ import React from 'react';
 import styles from '../styles/about.module.css';
 import Footer from '@/components/Footer';
 import Title from '@/components/Title';
+import Image from 'next/image';
 
 const About = () => {
   return (
     <div className={styles.mainContainer}>
       <Title />
-      {/* <div className={styles.bodyWrapper}> */}
-        <div className={styles.aboutContainer}>
-          <p className={styles.text}>
-            Who I am
-            <br />
-            <br />
-            My name is Jean-Marc Mazzoni, I am photographer concentrating in
-            food photography. I have a background in still life painting with a
-            passion for color with influences rooted in the Russian
-            impressionist and realist tradition. Besides this I continue to
-            practice many aspect of photography: portraits, pet portraits,
-            street photography, nature, B&W. I also continue to paint and
-            sculpt. Additionally, I speak English, French and Russian.
-            <br />
-            <br />
-            <br />
-            Where I am
-            <br />
-            <br />
-            San Mateo, San Francisco Peninsula, Bay Area, California
-          </p>
-
-          <br />
-          <br />
-          <br />
-          <p>
-            My History
-            <br />
-            <br />
-            ​I was born and raised in Paris where I first studied and practiced
-            medicine, then switched to information technology and moved to San
-            Francisco and Silicon Valley, California, where I now live, work and
-            continue to study.
-            <br />
-            <br />
-          </p>
-          <p>
-            I am also a licensed commercial drone pilot under FAA regulations.
-          </p>
+      <div className={styles.bodyWrapper}>
+        <div className={styles.photoWrapper}>
+        <Image src='/JM_10266_FINAL_w2.jpg' width={400} height={400} alt='About'  />
         </div>
-      {/* </div> */}
+      <div className={styles.aboutContainer}>
+        <p className={styles.text}>
+          Who I am
+          <br />
+          <br />
+          I am a food photographer based in San Francisco. I take bold colorful
+          rich in contrast photographs that embrace the rich cultural background
+          of diverse subjects while devoting special attention to color
+          temperature, sculpture presence and composition. My inspiration is
+          rooted in still life painting, with an emphasis on Impressionism,
+          French and Russian.
+          <br />
+          <br />
+          <br />
+          A bit more
+          <br />
+          Besides this I continue to practice many aspect of photography:
+          portraits, pet portraits, street photography, nature, B&W. I also
+          continue to paint and sculpt. Additionally, I speak English, French
+          and Russian.
+          <br />
+          <br />
+          San Mateo, San Francisco Peninsula, Bay Area, California
+        </p>
+
+        <br />
+        <br />
+        <br />
+        <p>
+          My History
+          <br />
+          <br />
+          I was born and raised in Paris where I first studied and practiced
+          medicine, then switched to information technology and moved to San
+          Francisco and Silicon Valley, California, where I now live, work and
+          continue to study.
+          <br />
+          <br />
+          Additionally, I speak English, French and Russian.
+        </p>
+        <br />
+        <p>
+          I am also a licensed commercial drone pilot under FAA regulations.
+        </p>
+      </div>
+      </div>
       {/* <Footer /> */}
     </div>
   );
